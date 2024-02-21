921a3bc471ff74137286673dbf023350ea188025
Mohamed Zarka <medzarka@gmail.com>
2024-02-21 17:01:25 +0300

    update virtual env path for routeros

--- routeros/vm_setup.sh
+++ routeros/vm_setup.sh
@@ -3,9 +3,9 @@ sudo pip3 install routeros_api
 
 #### install python virtualenv
 apt install python3.11-venv
-python3 -m venv .pyenv
-python_bin=.pyenv/bin/python
-pip_bin=.pyenv/bin/pip
+python3 -m venv /root/.pyenv
+python_bin=/root/.pyenv/bin/python
+pip_bin=/root/.pyenv/bin/pip
 
 #### Install script
 wget "https://download.mikrotik.com/routeros/7.11.2/chr-7.11.2.img.zip"
