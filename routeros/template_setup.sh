54062364d924712164c378b64e6ebd17dd0d2136
Mohamed Zarka <medzarka@gmail.com>
2024-02-21 19:20:43 +0300

    add routeros template script

---
+++ routeros/template_setup.sh
@@ -0,0 +1,60 @@
+#### install python virtualenv
+apt install python3.11-venv
+python3 -m venv /root/.pyenv
+python_bin=/root/.pyenv/bin/python
+pip_bin=/root/.pyenv/bin/pip
+
+#######################################
+routeros_version=7.13.5
+routeros_img_dir=/root
+template_vm_id=10000
+#######################################
+
+#### Install script
+#https://download.mikrotik.com/routeros/7.13.5/chr-7.13.5.img.zip
+routeros_img_name=chr-${routeros_version}.img.zip
+routeros_img_url=https://download.mikrotik.com/routeros/${routeros_version}/$routeros_img_name
+routeros_img_path=$routeros_img_dir/$routeros_img_name
+routeros_unzipped_image_path=/root/chr-${routeros_version}.img
+template_name="template-routeros-${routeros_version}"
+template_tags="_template,os_routeros,v_${routeros_version}"
+
+echo ''
+echo "### Download routeros image (version $routeros_version)..."
+echo ''
+if [ -e $routeros_unzipped_image_path ]
+then
+    echo "The routeros image file is already downloaded."
+else
+    echo "Downloading the routeros image file"
+    wget $routeros_img_url -O $routeros_img_path
+    unzip $routeros_img_path -d $routeros_img_dir
+fi
+
+echo ''
+echo "### Create the proxmox virtual machine with ID=$template_vm_id..."
+echo ''
+qm destroy $template_vm_id
+qm create $template_vm_id --name $template_name --ostype l26
+qm set $template_vm_id --net0 virtio,bridge=vmbr0
+qm set $template_vm_id --net1 virtio,bridge=vmbr1
+qm set $template_vm_id --serial0 socket --vga serial0
+qm set $template_vm_id --memory 256 --cores 2 --cpu host
+qm set $template_vm_id --machine q35 
+qm set $template_vm_id --scsi0 local-lvm:0,import-from="/root/chr-7.11.2.img",discard=on
+qm set $template_vm_id --boot order=scsi0 --scsihw virtio-scsi-single
+qm set $template_vm_id --tablet 0 
+qm set $template_vm_id --tags $template_tags 
+qm disk resize $template_vm_id scsi0 512M
+qm template $template_vm_id
+
+echo ''
+echo '### Cleaning ...'
+echo ''
+rm -f $routeros_unzipped_image_path
+rm -f $routeros_img_path
+
+echo ''
+echo '### Done.'
+echo ''
+
--- routeros/vm_setup.sh
+++ routeros/vm_setup.sh
@@ -4,9 +4,30 @@ python3 -m venv /root/.pyenv
 python_bin=/root/.pyenv/bin/python
 pip_bin=/root/.pyenv/bin/pip
 
+#######################################
+routeros_version=7.13.5
+routeros_img_dir=/root
+VMID=
+#######################################
+
 #### Install script
-wget "https://download.mikrotik.com/routeros/7.11.2/chr-7.11.2.img.zip" -O /root/chr-7.11.2.img.zip
-unzip /root/chr-7.11.2.img.zip -d /root
+#https://download.mikrotik.com/routeros/7.13.5/chr-7.13.5.img.zip
+routeros_img_name=chr-${routeros_version}.img.zip
+routeros_img_url=https://download.mikrotik.com/routeros/${routeros_version}/$routeros_img_name
+routeros_img_path=$routeros_img_dir/$routeros_img_name
+routeros_unzipped_image_path=/root/chr-${routeros_version}.img
+
+echo '### Download routeros image ...'
+if [ -e $routeros_unzipped_image_path ]
+then
+    echo "The routeros image file is already downloaded."
+else
+    echo "Downloading the routeros image file"
+    wget $routeros_img_url -O $routeros_img_path
+    unzip $routeros_img_path -d $routeros_img_dir
+fi
+
+echo '### Create the proxmox virtual machine ...'
 qm destroy 101
 qm create 101 --name "temp-mikrotik-ros-7.11.2" --ostype l26
 qm set 101 --net0 virtio,bridge=vmbr0
@@ -16,7 +37,7 @@ qm set 101 --memory 256 --cores 2 --cpu host
 qm set 101 --scsi0 local-lvm:0,import-from="/root/chr-7.11.2.img",discard=on
 qm set 101 --boot order=scsi0 --scsihw virtio-scsi-single
 qm disk resize 101 scsi0 1G
-qm template 101
+#qm template 101
 
 #### Config script
 $pip_bin install routeros_api
